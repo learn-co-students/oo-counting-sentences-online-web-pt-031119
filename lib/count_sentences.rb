dc683f4823f98dc0e35a320ba02ca3560ab3a3aa
AJ Guiles <ashiguiles@gmail.com>
2019-04-03 04:39:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
+
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    sentences=[]
+    self.split(/\?|\.|!/).each do |sentence|
+      sentences << sentence
+    end
+      sentences.reject { |sentence| sentence.empty? }
+    end
+     sentences.size
   end
+  
 end
+
+cities.reject { |c| c.empty? }
